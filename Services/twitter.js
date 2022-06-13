31374d00306cd39d25212954bea319990d3b8977
SergioAndes <54184733+SergioAndes@users.noreply.github.com>
2022-06-13 00:44:38 -0500

    se quema el toquen

--- Services/twitter.js
+++ Services/twitter.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 import dotenv from 'dotenv'
 
 dotenv.config();
-const twitterToken = process.env.TWITTER_BEARER_TOKEN
+const twitterToken = 'AAAAAAAAAAAAAAAAAAAAAAsXdgEAAAAAaZDyM0cjiCDIjt%2BwM%2BsYPKy7s8w%3Dxcc3PwgH51lU3NKAK2iB4Tl7qNlvNel0gCQsgBPJqo8LNIFiNz'
 
 const getTweets = async (userName)=>{
     const response = await fetch('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name='+userName+'&count=5&tweet_mode=extended',{
